38f6401c293e65690ab9efe68877fc81bd7c4b5f
BHASKAR_S_P <b29911904@gmail.com>
2026-08-22 22:30:44 +0530

    Enhance comments on blocking assignment in Verilog
    Added comments explaining blocking assignment in Verilog.

--- Verilog_Language/Basics/04_Four_wires.v
+++ Verilog_Language/Basics/04_Four_wires.v
@@ -6,5 +6,9 @@ module top_module(
     assign y=b;
     assign z=c;
     //   assign {w,x,y,z} = { a,b,b,c};    //using concatation
+    // although we call result = function is called blocking assignment 
+    // an blocking assignment (=) telles the conections or relation between operads that is 
+    // perticularly used in combinational circuit design 
+    // Sequntial assignment (<=) will be explained later  
 
 endmodule
