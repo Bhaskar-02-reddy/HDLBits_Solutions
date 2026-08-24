4b21c5e715e45665c1bc66b005a0d4c38b1e9ad4
BHASKAR_S_P <b29911904@gmail.com>
2026-08-24 22:52:01 +0530

    Change AND operation to logical-AND for clarity
    Updated AND gate assignment to use logical-AND operator from old Boolean and operator

--- Verilog_Language/Basics/06_AND_gate.v
+++ Verilog_Language/Basics/06_AND_gate.v
@@ -2,6 +2,7 @@ module top_module(
     input a, 
     input b, 
     output out );
-    assign out = a & b;
-
+    assign out = a && b; // make sures that operation matches output width size(one bit in this case) 
+    // Verilog has separate bitwise-AND (&) and logical-AND (&&) operators, 
+    // like C. Since we're working with a one-bit here, it doesn't matter which we choose.
 endmodule
