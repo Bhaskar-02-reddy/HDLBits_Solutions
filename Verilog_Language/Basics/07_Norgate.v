38a5e1b9d5a1be015b2e217534e86e168ba3e7b1
BHASKAR_S_P <b29911904@gmail.com>
2026-08-25 12:50:24 +0530

    Enhance comments in Norgate module
    Added comments to clarify the use of bitwise-OR and logical-OR operators in Verilog.

--- Verilog_Language/Basics/07_Norgate.v
+++ Verilog_Language/Basics/07_Norgate.v
@@ -3,5 +3,7 @@ module top_module(
     input b, 
     output out );
     assign out = ~ (a | b);  // Nand is the complement of and 
+   // Verilog has separate bitwise-OR (|) and logical-OR (||) operators, 
+   // like C. Since we're working with a one-bit here, it doesn't matter which we choose.
     
 endmodule
