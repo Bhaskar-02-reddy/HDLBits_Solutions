9695e5f8e1e31fd0d6194a0bd23947ee7605bf5c
BHASKAR_S_P <b29911904@gmail.com>
2026-08-27 23:17:46 +0530

    Refactor top_module with new input and output logic
    Updated the top_module to include additional logic for inputs a, b, and c, and added output assignments.

--- Verilog_Language/Basics/09_Wire_decl.v
+++ Verilog_Language/Basics/09_Wire_decl.v
@@ -1,4 +1,22 @@
-`default_nettype none
+
+// EXAMPLE CODE THAT'S EXPLAINS THE USE OF WIRE FOR AN INVERTER 
+
+module top_module (
+    input in,              // Declare an input wire named "in"
+    output out             // Declare an output wire named "out"
+);
+
+    wire not_in;           // Declare a wire named "not_in"
+
+    assign out = ~not_in;  // Assign a value to out (create a NOT gate).
+    assign not_in = ~in;   // Assign a value to not_in (create another NOT gate).
+
+endmodule   // End of module "top_module"
+
+
+
+
+
 module top_module(
     input a,
     input b,
@@ -15,5 +33,7 @@ module top_module(
     assign w3=w1|w2;
     assign out=w3;
     assign out_n=~w3;
+
+    
 
 endmodule
