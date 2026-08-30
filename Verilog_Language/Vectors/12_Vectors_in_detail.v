9e2459a99968575203f51296b7e4057e13eb0cd1
BHASKAR_S_P <b29911904@gmail.com>
2026-08-30 19:34:01 +0530

    Clarify part-select usage in byte ordering assignment
    Added comment to clarify the use of part-select in assignment.

--- Verilog_Language/Vectors/12_Vectors_in_detail.v
+++ Verilog_Language/Vectors/12_Vectors_in_detail.v
@@ -7,7 +7,7 @@ module top_module(
     // A 32-bit vector can be viewed as containing 4 bytes (bits [31:24], [23:16], etc.).
     // Build a circuit that will reverse the byte ordering of the 4-byte word
     
-    assign out_hi = in [15:8];  
+    assign out_hi = in [15:8]; // Part-select can be used on both the left side and right side of an assignment.
     assign out_lo = in [7:0];
 
 endmodule
