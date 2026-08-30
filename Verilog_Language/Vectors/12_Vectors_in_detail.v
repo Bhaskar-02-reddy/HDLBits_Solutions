b0d29ebc0f28ffeb32b6cc705fe67136d86611a3
BHASKAR_S_P <b29911904@gmail.com>
2026-08-30 19:30:57 +0530

    Fix duplicate assignment for out_hi

--- Verilog_Language/Vectors/12_Vectors_in_detail.v
+++ Verilog_Language/Vectors/12_Vectors_in_detail.v
@@ -3,8 +3,11 @@ module top_module(
     input wire [15:0] in,
     output wire [7:0] out_hi,
     output wire [7:0] out_lo );
+
+    // A 32-bit vector can be viewed as containing 4 bytes (bits [31:24], [23:16], etc.).
+    // Build a circuit that will reverse the byte ordering of the 4-byte word
     
-    assign out_hi = in [15:8];
+    assign out_hi = in [15:8];  
     assign out_lo = in [7:0];
 
 endmodule
