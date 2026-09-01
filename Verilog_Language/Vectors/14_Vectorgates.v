ae40c05572bee3aa24d824b052973b38659093cb
BHASKAR_S_P <b29911904@gmail.com>
2026-09-01 15:12:07 +0530

    Add comments to vector gate assignments
    Earlier, we mentioned that there are bitwise and logical versions of the various boolean operators (e.g., norgate). When using vectors, the distinction between the two operator types becomes important. A bitwise operation between two N-bit vectors replicates the operation for each bit of the vector and produces a N-bit output, while a logical operation treats the entire vector as a boolean value (true = non-zero, false = zero) and produces a 1-bit output.

--- Verilog_Language/Vectors/14_Vectorgates.v
+++ Verilog_Language/Vectors/14_Vectorgates.v
@@ -5,9 +5,9 @@ module top_module(
     output out_or_logical,
     output [5:0] out_not
 );
-    assign out_or_bitwise = a | b;
-    assign out_or_logical = a || b;
-    assign out_not[5:3] = ~b;
-    assign out_not[2:0] = ~a;
+    assign out_or_bitwise = a | b;    // butwise or
+    assign out_or_logical = a || b;   // logical or
+    assign out_not[5:3] = ~b;     // inverter b for msb(most significant bit)
+    assign out_not[2:0] = ~a;     // inverter a for lsb(liest significant bit)
 
 endmodule
