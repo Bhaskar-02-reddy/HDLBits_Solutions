bed28c815bf930577e260dddb8f1e19bc7b4cbec
BHASKAR_S_P <b29911904@gmail.com>
2026-09-01 15:17:40 +0530

    Fix comments for consistency in Vectorgates.v
    added the hint part to make the things clear and readable

--- Verilog_Language/Vectors/14_Vectorgates.v
+++ Verilog_Language/Vectors/14_Vectorgates.v
@@ -1,3 +1,7 @@
+// HINT PROVIDED :
+// Even though you cannot assign to a wire more than once,
+// you can use a part select on the left-hand-side of an assign.
+// You don't need to assign to the entire vector all in one statement.
 module top_module( 
     input [2:0] a,
     input [2:0] b,
@@ -7,7 +11,7 @@ module top_module(
 );
     assign out_or_bitwise = a | b;    // butwise or
     assign out_or_logical = a || b;   // logical or
-    assign out_not[5:3] = ~b;     // inverter b for msb(most significant bit)
-    assign out_not[2:0] = ~a;     // inverter a for lsb(liest significant bit)
+    assign out_not[5:3] = ~b;     // inverter b for MSB(most significant bit)
+    assign out_not[2:0] = ~a;     // inverter a for LSB(liest significant bit)
 
 endmodule
