3d4022c6c770c1ca6426704a6220c50b03d2b821
BHASKAR_S_P <b29911904@gmail.com>
2026-09-02 17:15:21 +0530

    Add comments for gate outputs in 15_Gates4.v
    Added comments to clarify the outputs of the gates.

--- Verilog_Language/Vectors/15_Gates4.v
+++ Verilog_Language/Vectors/15_Gates4.v
@@ -4,8 +4,9 @@ module top_module(
     output out_or,
     output out_xor
 );
-    assign out_and = & in;
-    assign out_or = | in;
-    assign out_xor = ^in;
+    assign out_and = & in;   // out_and: output of a 4-input AND gate.
+    assign out_or = | in;    // out_or: output of a 4-input OR gate.
+    assign out_xor = ^in;    // out_xor: output of a 4-input XOR gate.
+
 
 endmodule
