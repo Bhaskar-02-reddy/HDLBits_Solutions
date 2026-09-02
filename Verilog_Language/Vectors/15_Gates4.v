06028626854bd517e837c72387b632e2ef3faafb
BHASKAR_S_P <b29911904@gmail.com>
2026-09-02 17:28:54 +0530

    Enhance comments for bitwise operations in 15_Gates4.v
    Added comments to clarify the functionality of the gates.

--- Verilog_Language/Vectors/15_Gates4.v
+++ Verilog_Language/Vectors/15_Gates4.v
@@ -4,9 +4,11 @@ module top_module(
     output out_or,
     output out_xor
 );
+    // BITWISE OPERATIONON FOR ONE BIT RESULT . 
     assign out_and = & in;   // out_and: output of a 4-input AND gate.
     assign out_or = | in;    // out_or: output of a 4-input OR gate.
     assign out_xor = ^in;    // out_xor: output of a 4-input XOR gate.
+    
 
 
 endmodule
