97cea9f7f8c7973771b4f8aa2e9a0f0703df3b27
BHASKAR_S_P <b29911904@gmail.com>
2026-09-03 13:46:09 +0530

    Clean up commented code in top_module
    Removed commented-out code related to temporary variable.

--- Verilog_Language/Vectors/16_Vector3.v
+++ Verilog_Language/Vectors/16_Vector3.v
@@ -1,7 +1,7 @@
 module top_module (
     input [4:0] a, b, c, d, e, f,
     output [7:0] w, x, y, z );//
-    /*wire [31:0]tatal;
+    /*wire [31:0]tatal;      //THIS METHOD INTRODUCES AN TEMPROVARY VARIABLE THAT THREDS TO RESPECTVE OUTPUT PORTS
     //assign tatal = { a,b,c,d,e,f,2'b11};
     //assign w = tatal[31:24];
     //assign x = tatal[23:16];
