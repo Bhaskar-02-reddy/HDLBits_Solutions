416724e5231d3541fc49bae254f39226a9268046
BHASKAR_S_P <b29911904@gmail.com>
2026-09-03 13:38:38 +0530

    Refactor comments and update concatenation assignment
    Commented out unused wire declaration and assignment logic.

---
+++ Verilog_Language/Vectors/16_Vector3.v
@@ -0,0 +1,15 @@
+module top_module (
+    input [4:0] a, b, c, d, e, f,
+    output [7:0] w, x, y, z );//
+    /*wire [31:0]tatal;
+    //assign tatal = { a,b,c,d,e,f,2'b11};
+    //assign w = tatal[31:24];
+    //assign x = tatal[23:16];
+    //assign y = tatal[15:8];
+    //assign z = tatal[7:0];
+     THE ABOW ONE IS BIG BUT CLEAR STEP ,
+  below one is one line concatination that sets automatically unless the siz'z is same */
+
+    assign {w,x,y,z } = {a,b,c,d,e,f,2'b11  };
+
+endmodule
