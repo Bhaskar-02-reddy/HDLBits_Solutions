cd6c52093690ed14d6d3762359335412ed132bd3
BHASKAR_S_P <b29911904@gmail.com>
2026-09-04 07:49:03 +0530

    Enhance comments for bit reversal module
    Added comments explaining the functionality and potential improvements for reversing an 8-bit vector.

--- Verilog_Language/Vectors/17_vectorr.v
+++ Verilog_Language/Vectors/17_vectorr.v
@@ -1,5 +1,5 @@
 // Given an 8-bit input vector [7:0], reverse its bit ordering
-
+// we can also use an looping structure under for or generate for large bit reversing . 
 
 module top_module( 
     input [7:0] in,
