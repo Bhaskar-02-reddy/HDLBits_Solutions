0ef01b612a5c95f6e2b8bd2297aae559cfbb06b8
BHASKAR_S_P <b29911904@gmail.com>
2026-09-05 18:57:50 +0530

    Refactor output assignment and add comments
    Updated the assignment of 'out' to include comments about the replication operator.

--- Verilog_Language/Vectors/18_vector4.v
+++ Verilog_Language/Vectors/18_vector4.v
@@ -1,7 +1,15 @@
 module top_module (
     input [7:0] in,
     output [31:0] out );//
-    assign out  = { {24{in[7]}} , in };
+  
     // assign out = { replicate-sign-bit , the-input };
-
+       assign out  = { {24{in[7]}} , in };
 endmodule
+
+/* The concatenation operator allowed concatenating together vectors to form a larger vector.
+But sometimes you want the same thing concatenated together many times,
+and it is still tedious to do something like assign a = {b,b,b,b,b,b};.
+The replication operator allows repeating a vector and concatenating them together:  */
+
+
+
