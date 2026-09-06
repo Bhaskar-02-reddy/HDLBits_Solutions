0ce5deb246f18244697244c94801f47a6db26e85
BHASKAR_S_P <b29911904@gmail.com>
2026-09-06 15:14:21 +0530

    Fix output assignment and update comments in 19_Vector5.v
    Corrected the commented-out code and clarified the output assignment.

--- Verilog_Language/Vectors/19_Vector5.v
+++ Verilog_Language/Vectors/19_Vector5.v
@@ -1,11 +1,12 @@
 module top_module (
     input a, b, c, d, e,
     output [24:0] out );//
-    assign out = ~ { {5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}} } ^ { 5{a,b,c,d,e}};
-    //assign out = ~  { a, b, c, d, e} ^ {5{a,b,c,d,e}} ; wrong because size are not matching 
-  // assign out = ~  { a, b, c, d, e} ^ { {5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}} }  ;  //{ {5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}} } ;
+
     // The output is XNOR of two vectors created by 
     // concatenating and replicating the five inputs.
     // assign out = ~{ ... } ^ { ... };
 
+      
+    //assign out = ~  { a, b, c, d, e} ^ {5{a,b,c,d,e}} ; wrong because size are not matching
+    assign out = ~ { {5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}} } ^ { 5{a,b,c,d,e}};
 endmodule
