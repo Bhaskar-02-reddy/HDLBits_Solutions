ebb573415e7a372f5272aa82c5100d0ecfe02f1f
BHASKAR_S_P <b29911904@gmail.com>
2026-09-06 15:15:15 +0530

    Add comments for vector pairwise comparison module
    Added module documentation for pairwise comparisons.

--- Verilog_Language/Vectors/19_Vector5.v
+++ Verilog_Language/Vectors/19_Vector5.v
@@ -1,3 +1,7 @@
+/* Given five 1-bit signals (a, b, c, d, and e),
+compute all 25 pairwise one-bit comparisons in the 25-bit output vector.
+The output should be 1 if the two bits being compared are equal */
+
 module top_module (
     input a, b, c, d, e,
     output [24:0] out );//
