e75eede09fb989dd8ba8edee2b82878818281de4
BHASKAR_S_P <b29911904@gmail.com>
2026-09-10 06:07:10 +0530

    Add comments to top_module instances
    Added comments to clarify the functionality of each instance in the top_module.

--- Verilog_Language/Modules:Hierarchy/23_Three_modules.v
+++ Verilog_Language/Modules:Hierarchy/23_Three_modules.v
@@ -1,7 +1,7 @@
 module top_module ( input clk, input d, output q );
     wire w1,w2;
-    my_dff instance1(.clk(clk), .d(d), .q(w1));                             
-    my_dff instance2(.clk(clk), .d(w1), .q(w2));                      
-    my_dff instance3(.clk(clk), .d(w2), .q(q) );             
+    my_dff instance1(.clk(clk), .d(d), .q(w1));     // Name calling                        
+    my_dff instance2(.clk(clk), .d(w1), .q(w2));    // data is shifing from one module to next                   
+    my_dff instance3(.clk(clk), .d(w2), .q(q) );    // data -> w1 -> w2 -> q        
  
 endmodule
