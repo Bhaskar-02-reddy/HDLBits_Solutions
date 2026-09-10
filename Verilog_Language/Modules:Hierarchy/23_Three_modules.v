112e34f13eb3d2f65c84e32b77e6c1a695053d08
BHASKAR_S_P <b29911904@gmail.com>
2026-09-10 06:08:24 +0530

    Enhance comments for wire and module instance clarity
    Added comments to clarify wire declarations and module instance naming.

--- Verilog_Language/Modules:Hierarchy/23_Three_modules.v
+++ Verilog_Language/Modules:Hierarchy/23_Three_modules.v
@@ -1,3 +1,7 @@
+
+/* Note that to make the internal connections, you will need to declare some wires.
+Be careful about naming your wires and module instances: the names must be unique */
+
 module top_module ( input clk, input d, output q );
     wire w1,w2;
     my_dff instance1(.clk(clk), .d(d), .q(w1));     // Name calling                        
