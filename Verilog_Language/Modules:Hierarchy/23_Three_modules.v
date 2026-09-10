9d610a53a2bad20dcb58b83ccfe7fe1afee579c5
BHASKAR_S_P <b29911904@gmail.com>
2026-09-10 06:08:58 +0530

    Enhance comments for clarity in top_module
    Added comments to clarify wire connections and data flow.

--- Verilog_Language/Modules:Hierarchy/23_Three_modules.v
+++ Verilog_Language/Modules:Hierarchy/23_Three_modules.v
@@ -3,7 +3,7 @@
 Be careful about naming your wires and module instances: the names must be unique */
 
 module top_module ( input clk, input d, output q );
-    wire w1,w2;
+    wire w1,w2; // wire that connects one module signal to another 
     my_dff instance1(.clk(clk), .d(d), .q(w1));     // Name calling                        
     my_dff instance2(.clk(clk), .d(w1), .q(w2));    // data is shifing from one module to next                   
     my_dff instance3(.clk(clk), .d(w2), .q(q) );    // data -> w1 -> w2 -> q        
