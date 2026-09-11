5bfa746c1ec5d70c61ed21cadca181178a8d6a83
BHASKAR_S_P <b29911904@gmail.com>
2026-09-11 06:02:23 +0530

    Enhance comments for clarity in 24_Modules_and_vectors.v
    Added comments to clarify signal connections and multiplexer behavior.

--- Verilog_Language/Modules:Hierarchy/24_Modules_and_vectors.v
+++ Verilog_Language/Modules:Hierarchy/24_Modules_and_vectors.v
@@ -13,13 +13,13 @@ module top_module (
     input [1:0] sel, 
     output [7:0] q 
 );
-    wire [7:0]w1,w2,w3;
+    wire [7:0]w1,w2,w3;    // signals that connects one port to another
     my_dff8 instance1(.clk(clk), .d(d), .q(w1));  
     my_dff8 instance2(.clk(clk), .d(w1), .q(w2) );
     my_dff8 instance3(.clk(clk), .d(w2), .q(w3) );
-    always @(*)
+    always @(*)    // (*) includes all the conditions changes  
         begin
-            case(sel)
+            case(sel)      // 4:1 Multiplexer
                 2'b00 : q = d;
                 2'b01 : q = w1;
                 2'b10 : q = w2;
