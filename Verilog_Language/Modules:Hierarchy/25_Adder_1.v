af96f00c69745acf1fcb93b8addc3ab351ffc544
BHASKAR_S_P <b29911904@gmail.com>
2026-09-12 18:26:40 +0530

    Clarify sum assignment in top_module
    Updated comments to clarify assignment methods for sum.

--- Verilog_Language/Modules:Hierarchy/25_Adder_1.v
+++ Verilog_Language/Modules:Hierarchy/25_Adder_1.v
@@ -1,3 +1,6 @@
+
+
+
 module top_module(
     input [31:0] a,
     input [31:0] b,
@@ -7,8 +10,8 @@ module top_module(
     wire c1;
     add16 instance1( .a(a[15:0]), .b(b[15:0]), .sum(w1), .cin(0), .cout(c1) );
     add16 instance2(.a(a[31:16]), .b(b[31:16]), .sum(w2), .cin(c1), .cout(1'bz) );
-    assign sum[15:0] = w1;
-    assign sum[31:16] = w2 ;
+    assign sum[15:0] = w1;     // other way is one line assign with concatination
+    assign sum[31:16] = w2 ;    // {sum[15:0],sum[31:16]} = { w1,w2};
     
 
 endmodule
