5b57a32ca4312e90a80fd70d232e7f4732f675b0
BHASKAR_S_P <b29911904@gmail.com>
2026-09-14 08:08:51 +0530

    Add comments for carry-select adder explanation
    Added comments explaining the drawbacks of ripple carry adders and the improvements of carry-select adders.

--- Verilog_Language/Modules:Hierarchy/27_Carry_select_adder.v
+++ Verilog_Language/Modules:Hierarchy/27_Carry_select_adder.v
@@ -1,3 +1,13 @@
+/* One drawback of the ripple carry adder (See previous exercise) is that the delay 
+for an adder to compute the carry out (from the carry-in, 
+in the worst case) is fairly slow, and the second-stage 
+adder cannot begin computing its carry-out until the first-stage adder has finished.
+This makes the adder slow. One improvement is a carry-select adder, shown below. 
+The first-stage adder is the same as before, but we duplicate the second-stage adder, 
+one assuming carry-in=0 and one assuming carry-in=1,
+then using a fast 2-to-1 multiplexer to select which result happened to be correct. */
+
+
 module top_module(
     input [31:0] a,
     input [31:0] b,
