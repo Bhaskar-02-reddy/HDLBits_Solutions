d394eb8861fd892323d9d378b795c428b85f6f41
BHASKAR_S_P <b29911904@gmail.com>
2026-09-15 18:41:45 +0530

    Add explanation for adder-subtractor module
    Added a comment explaining the adder-subtractor functionality.

--- Verilog_Language/Modules:Hierarchy/28_Adder_subtractor.v
+++ Verilog_Language/Modules:Hierarchy/28_Adder_subtractor.v
@@ -1,3 +1,9 @@
+/* An adder-subtractor can be built from an adder by optionally negating one of the inputs, 
+which is equivalent to inverting the input then adding 1.
+The net result is a circuit that can do two operations: (a + b + 0) and (a + ~b + 1). 
+See Wikipedia if you want a more detailed explanation of how this circuit works */
+
+
 module top_module(
     input [31:0] a,
     input [31:0] b,
