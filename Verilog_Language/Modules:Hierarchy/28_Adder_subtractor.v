b1342840e7df1bbacb11638fc06a6d135201af57
BHASKAR_S_P <b29911904@gmail.com>
2026-09-15 18:43:20 +0530

    Clarify wire c1 purpose in adder-subtractor module
    Added a comment to clarify the purpose of wire c1.

--- Verilog_Language/Modules:Hierarchy/28_Adder_subtractor.v
+++ Verilog_Language/Modules:Hierarchy/28_Adder_subtractor.v
@@ -13,7 +13,7 @@ module top_module(
     //Use a 32-bit wide XOR gate to invert the b input whenever sub is 1.
     //(This can also be viewed as b[31:0] XORed with sub replicated 32 times. See replication operator.).
     //Also connect the sub input to the carry-in of the adder.
-    wire c1;
+    wire c1; // wire that connects( by providing carry) from one module of adder to another 
     add16 instance1( .a(a[15:0]), .b(b[15:0] ^ {16{sub}}),  .cin(sub), .cout(c1), .sum(sum[15:0]) );
     add16 instance2( .a(a[31:16]), .b(b[31:16] ^ {16{sub}}), .cin(c1), .cout(), .sum(sum[31:16])  );
 
