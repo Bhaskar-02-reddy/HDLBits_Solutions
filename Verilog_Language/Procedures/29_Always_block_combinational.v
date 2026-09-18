2fc34b783d3300597fcca1424b8661498a859ebd
BHASKAR_S_P <b29911904@gmail.com>
2026-09-18 05:54:03 +0530

    Enhance comments on always block usage in Verilog
    Added comments explaining the use of procedures in Verilog.

--- Verilog_Language/Procedures/29_Always_block_combinational.v
+++ Verilog_Language/Procedures/29_Always_block_combinational.v
@@ -1,4 +1,8 @@
 // synthesis verilog_input_version verilog_2001
+
+Since digital circuits are composed of logic gates connected with wires, any circuit can be expressed as some combination of modules and assign statements. However, sometimes this is not the most convenient way to describe the circuit. 
+    
+    Procedures (of which always blocks are one example) provide an alternative syntax for describing circuits
 module top_module(
     input a, 
     input b,
